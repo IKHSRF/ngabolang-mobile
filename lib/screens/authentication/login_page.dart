7a9fb75fe41b246721560b7428aff0f2578a524b
Ikhsan Arfian Nugraha <ikhsan3568@gmail.com>
2020-11-18 18:02:35 +0700

    add login page file

---
+++ lib/screens/authentication/login_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  static const String id = 'login_page';
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
