f6b494d793b7469a877281eb82dfa94995bcb249
TjandraD <tjandradarmo@protonmail.com>
2020-11-22 22:01:49 +0700

    Create maps screen & stream to current user location

---
+++ lib/screens/maps/maps_screen.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
+class MapsScreen extends StatefulWidget {
+  static final String id = 'maps_screen';
+
+  @override
+  State<MapsScreen> createState() => MapsScreenState();
+}
+
+class MapsScreenState extends State<MapsScreen> {
+  GoogleMapController _controller;
+  Position position;
+  StreamSubscription<Position> _locationSubscription;
+  static final CameraPosition _initialLocation = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 10,
+  );
+
+  void getCurrentLocation() async {
+    try {
+      if (_locationSubscription != null) {
+        _locationSubscription.cancel();
+      }
+
+      _locationSubscription =
+          Geolocator.getPositionStream(desiredAccuracy: LocationAccuracy.best)
+              .listen((position) {
+        if (_controller != null) {
+          _controller
+              .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+            target: LatLng(position.latitude, position.longitude),
+            zoom: 15,
+          )));
+        }
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: _initialLocation,
+        zoomControlsEnabled: false,
+        onMapCreated: (GoogleMapController controller) {
+          _controller = controller;
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => getCurrentLocation(),
+        child: Icon(
+          Icons.location_on_outlined,
+          color: Colors.black,
+        ),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
