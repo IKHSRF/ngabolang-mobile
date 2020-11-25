bcb1c282652deadef694a7b6ca648fd48d40c689
Ikhsan Arfian Nugraha <ikhsan3568@gmail.com>
2020-11-25 21:19:14 +0700

    fix typo

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: HomePage.id,
+      initialRoute: GetStartedPage.id,
       getPages: [
         GetPage(
           name: GetStartedPage.id,
