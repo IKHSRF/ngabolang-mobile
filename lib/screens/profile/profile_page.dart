7d951acc32e1195b9dfe6c2e3a34b54beb0dae72
TjandraD <tjandradarmo@protonmail.com>
2020-12-01 21:05:40 +0700

    Add a new route & create profile page

---
+++ lib/screens/profile/profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  static final String id = 'profile_page';
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
