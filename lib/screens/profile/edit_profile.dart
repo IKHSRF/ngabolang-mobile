065d3b2664b81da65a39f9b8a52063a55504ac4b
TjandraD <tjandradarmo@protonmail.com>
2020-12-08 21:27:03 +0700

    Create new file for edit user profile

---
+++ lib/screens/profile/edit_profile.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class EditProfile extends StatefulWidget {
+  static String id = 'edit_profile';
+  @override
+  _EditProfileState createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
