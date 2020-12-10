92a2a43462b0ee7f4ce1d94df4dea94ee2b3bb12
TjandraD <tjandradarmo@protonmail.com>
2020-12-10 12:03:20 +0700

    Add progress indicator while updating profile

--- lib/screens/profile/edit_profile.dart
+++ lib/screens/profile/edit_profile.dart
@@ -7,6 +7,7 @@ import 'package:ngabolang/widgets/post_field.dart';
 import 'package:ngabolang/widgets/user_image_picker.dart';
 import 'package:ngabolang/services/firebase_storage.dart';
 import 'package:ngabolang/services/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class EditProfile extends StatefulWidget {
   static String id = 'edit_profile';
@@ -17,6 +18,7 @@ class EditProfile extends StatefulWidget {
 class _EditProfileState extends State<EditProfile> {
   TextEditingController nameController = TextEditingController();
   User _user = FirebaseAuth.instance.currentUser;
+  bool showLoading = false;
   String imageUrl;
   String name;
   String email;
@@ -38,45 +40,55 @@ class _EditProfileState extends State<EditProfile> {
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 36.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            UserImagePicker(
-              screenSize: screenSize,
-              imageUrl: imageUrl == _user.photoURL ? null : imageUrl,
-              buttonTap: () async {
-                File file = await StorageServices.getImage();
-                imageUrl = await StorageServices.uploadPhoto(
-                    file, FirebaseAuth.instance.currentUser.uid);
-                setState(() {});
-              },
-            ),
-            PostField(
-              controller: nameController,
-              hintText: 'Full Name',
-              margin: 0,
-            ),
-            SizedBox(
-              height: 36,
-            ),
-            BlueButton(
-              screenSize: screenSize,
-              buttonTap: () async {
-                var result = await AuthServices.editUserProfile(
-                    nameController.text.trim(), imageUrl);
+      body: ModalProgressHUD(
+        inAsyncCall: showLoading,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 36.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              UserImagePicker(
+                screenSize: screenSize,
+                imageUrl: imageUrl == _user.photoURL ? null : imageUrl,
+                buttonTap: () async {
+                  File file = await StorageServices.getImage();
+                  imageUrl = await StorageServices.uploadPhoto(
+                      file, FirebaseAuth.instance.currentUser.uid);
+                  setState(() {});
+                },
+              ),
+              PostField(
+                controller: nameController,
+                hintText: 'Full Name',
+                margin: 0,
+              ),
+              SizedBox(
+                height: 36,
+              ),
+              BlueButton(
+                screenSize: screenSize,
+                buttonTap: () async {
+                  setState(() {
+                    showLoading = !showLoading;
+                  });
+                  var result = await AuthServices.editUserProfile(
+                      nameController.text.trim(), imageUrl);
 
-                if (result != 'berhasil') {
-                  Get.snackbar('Opps Something went wrong', result,
-                      colorText: Colors.white, backgroundColor: Colors.red);
-                } else {
-                  Get.back();
-                }
-              },
-              buttonText: 'Edit',
-            ),
-          ],
+                  setState(() {
+                    showLoading = !showLoading;
+                  });
+
+                  if (result != 'berhasil') {
+                    Get.snackbar('Opps Something went wrong', result,
+                        colorText: Colors.white, backgroundColor: Colors.red);
+                  } else {
+                    Get.back();
+                  }
+                },
+                buttonText: 'Edit',
+              ),
+            ],
+          ),
         ),
       ),
     );
